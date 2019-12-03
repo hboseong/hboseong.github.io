f1e06dcd3d4928afebdadbd4d26ab6e77a08e226
BoseongHwang <BoseongHwang@BOSEONG-1>
2019-12-03 12:54:35 +0900

    js test

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
 
 window.onload = function() { 
 
-	alert("window"); 
-	alert('iframe 객체## append' +  $('.iframely-embed').find('iframe').find('head').append("<link rel='stylesheet' href='{{'/assets/css/gist.css'}}''>"));
+	alert('iframe appen시도##  ');
+	$('.iframely-embed').find('iframe').find('head').append("<link rel='stylesheet' href='{{'https://hboseong.github.io/assets/css/gist.css'}}''>");
 	// $('iframe').load( function() {
 	// 	alert($('iframe').contents().find('head'));
 //    .append($("<style type='text/css'>  .my-class{display:none;}  </style>"));
